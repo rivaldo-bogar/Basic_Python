a40d7def15258cab0ccc657e9614981c923938c4
rivaldo-bogar <androsbgr13@gmail.com>
2023-08-12 16:08:03 +0800

    feat : new code cover value

--- variabel.py
+++ variabel.py
@@ -1,2 +1,13 @@
-# learn basic
+# learn variabel basic
+
+
 a = 50;
+
+print("Nilai a = ",a)
+
+
+b = a; # operator indirect = menginisialisasi variabel baru dengan variabel sebelumnya.
+print("Nilai b = ",b)
+
+a = 10; # cover new value to vairiabel a
+print("Nilai a = ",a)
