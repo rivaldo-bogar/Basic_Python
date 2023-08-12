d080382b95c2f563ec1aeb84f4100feccacc8b75
rivaldo-bogar <androsbgr13@gmail.com>
2023-08-12 19:10:22 +0800

    feat : make new file

---
+++ first_Syntax.py
@@ -0,0 +1,1 @@
+print("Bogar Rivaldo")
--- variabel.py
+++ variabel.py
@@ -2,12 +2,8 @@
 
 
 a = 50;
-
 print("Nilai a = ",a)
-
-
 b = a; # operator indirect = menginisialisasi variabel baru dengan variabel sebelumnya.
 print("Nilai b = ",b)
-
 a = 10; # cover new value to vairiabel a
 print("Nilai a = ",a)
